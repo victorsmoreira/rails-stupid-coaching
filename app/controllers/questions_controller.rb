ca3e937be960d7dd3a012a9d03000c7cbd8f344e
Victor Moreira <victorsmoreira@outlook.com>
2021-08-16 12:10:04 -0300

    routes and controllers working. CSS pending

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Questions controller
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @answer =
+      if params['question'].downcase == 'i am going to work right now!'
+        'Great!'
+      elsif params['question'].end_with?('?')
+        'Silly question, get dressed and go to work!'
+      else
+        "I don't care, get dressed and go to work!"
+      end
+  end
+end
